1f0ccb064ae19acc8dc56db2dd4c8d614f4d8b67
Murugan <murugu2012@gmail.com>
2017-01-02 20:12:51 +0530

    Validate uniqueness of projects.name

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,6 +5,8 @@ class Project < ApplicationRecord
 
   # Validations
 
+  validates :name, :uniqueness => true
+
   validates :name, :presence => true
 
   validates :number_of_volunteers_required, :presence => true
