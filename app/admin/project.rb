6afde115c2e9b5c1054c9fb38457d705c02e5e15
Murugan <murugu2012@gmail.com>
2017-01-02 20:13:16 +0530

    Whitelist columns in Project for ActiveAdmin

--- app/admin/project.rb
+++ app/admin/project.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Project do
 
+ permit_params :name, :location_id, :day_id, :description, :number_of_volunteers_required, :organization_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
